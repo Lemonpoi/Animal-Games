c9192e35e4213565217dc3e14bb1c74f11187dea
PetrolMo <1850956@tongji.edu>
2020-11-16 17:27:53 +0800

    项目创建

---
+++ cmake-build-debug/CMakeFiles/AnimalGames.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AnimalGames"
+  "AnimalGames.pdb"
+  "CMakeFiles/AnimalGames.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnimalGames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
